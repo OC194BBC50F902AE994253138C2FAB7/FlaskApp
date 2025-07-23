4d87bd97aaf28ee293344a8b738e2ed119b18b66
Aruna <arathanakbca@gmail.com>
2025-07-23 13:58:43 +0530

    testvercel

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1> Hello World!!</h1> "
+    return "<center><h1> Hello Arathana !!</h1></center> "
 
 if __name__ == "__main__":
     app.run(debug = True)
